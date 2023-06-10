e989036d6cd5a31a7982577e00c1035cde51d618
rowgregory <rowgregory@gmail.com>
2023-06-09 20:06:51 -0400

    make api endpoint base dynamic

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -8,7 +8,7 @@ import { UserContext } from '../context/userContext.jsx';
 async function loginUser(email, password) {
   const body = { email, password };
   const response = await fetch(
-    'http://localhost:3000/api/user?endpoint=login',
+    `${process.env.VERCEL_BASE_URL}/api/user?endpoint=login`,
     {
       method: 'POST',
       headers: {
